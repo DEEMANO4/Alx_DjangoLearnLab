3a1d017f9e687807a2f0f1854f9923c9d231409b
DEEMANO4 <apenadavid18@gmail.com>
2025-11-09 12:04:09 +0100

    ..

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import LibraryDetailView, LibraryTemplateView, register
+from .views import list_books, LibraryDetailView, LibraryTemplateView, register
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
