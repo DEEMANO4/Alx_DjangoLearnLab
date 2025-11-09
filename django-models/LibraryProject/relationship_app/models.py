8ebe7ed1451d24609d23a227e2552db9dae8ab03
DEEMANO4 <apenadavid18@gmail.com>
2025-11-09 19:58:00 +0100

    ..

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -18,3 +18,12 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+class UserProfile(models.Model):
+    USER_ROLES = (
+        ('Admin'),
+        ('Librarian'),
+        ('Member'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=USER_ROLES)
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('library/', views.LibraryDetailView.as_view(), name='library_books'),
     path('library-template', views.LibraryTemplateView.as_view(), name='library_page'),
     path('register/', views.register, name='registration'),
-    path('login/', LoginView.as_view(template_name='templates/relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='templates/relationship_app/logout.html'), name= 'logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name= 'logout'),
     
     ]
