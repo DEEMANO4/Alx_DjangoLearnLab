10a35ba38fcdd22ac09bf4c2ada7a0f358425ab6
DEEMANO4 <apenadavid18@gmail.com>
2025-11-15 19:06:13 +0100

    ..

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -13,35 +13,35 @@ from django.contrib.auth.decorators import permission_required
 # from .forms import BookForm
 
 @permission_required('books.can_create_book', raise_exception=True)
-def book_create(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm()
-    return render(request, 'books/book_form.html', {'form': form})
+# def book_create(request):
+#     if request.method == 'POST':
+#         form = BookForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('book_list')
+#     else:
+#         form = BookForm()
+#     return render(request, 'books/book_form.html', {'form': form})
 
 @permission_required('books.can_edit_book', raise_exception=True)
-def book_edit(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'books/book_form.html', {'form': form})
+# def book_edit(request, pk):
+#     book = get_object_or_404(Book, pk=pk)
+#     if request.method == 'POST':
+#         form = BookForm(request.POST, instance=book)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('book_list')
+#     else:
+#         form = BookForm(instance=book)
+#     return render(request, 'books/book_form.html', {'form': form})
 
 @permission_required('books.can_delete_book', raise_exception=True)
-def book_delete(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('book_list')
-    return render(request, 'books/book_confirm_delete.html', {'book': book})
+# def book_delete(request, pk):
+#     book = get_object_or_404(Book, pk=pk)
+#     if request.method == 'POST':
+#         book.delete()
+#         return redirect('book_list')
+#     return render(request, 'books/book_confirm_delete.html', {'book': book})
 
 # This view might not require specific book-level permissions,
 # but rather general "view" permission if you want to restrict it.
