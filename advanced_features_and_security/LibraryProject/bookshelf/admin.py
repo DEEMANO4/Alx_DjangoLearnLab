9afd1307a1e48c2d1053e1c6587ea4355d553d84
DEEMANO4 <apenadavid18@gmail.com>
2025-11-15 13:40:24 +0100

    ..

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from.models import CustomUser, CustomUserManager
 
 # Register your models here.
-admin.site.register(Book)
+
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publicatiion_year')
     search_fields = ('title', 'author', 'publication_year')
+
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('date_of_birth',)
+    fieldsets = UserAdmin.fieldsets + (
+        ('Personal Info', {'fields': ('date_of_birth', 'profile_photo',)})
+    )
+
+admin.site.register(Book, CustomUser, CustomUserAdmin)
