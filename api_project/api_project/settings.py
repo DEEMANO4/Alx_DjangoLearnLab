18c4268527f485cf03921598fc8534d15bc431e5
DEEMANO4 <apenadavid18@gmail.com>
2025-11-21 11:50:58 +0100

    corrections

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authenticataion.TokenAuthentication',
+        "rest_framework.authenticataion.TokenAuthentication",
         'rest_framework.authentication.SessionAuthentication',
     ],
         
