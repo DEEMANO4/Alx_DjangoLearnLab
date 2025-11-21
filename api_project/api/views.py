1db26b4678d81b0b344d38bda70fbc117441e039
DEEMANO4 <apenadavid18@gmail.com>
2025-11-21 11:48:39 +0100

    Authentication and Permission

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,12 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -12,3 +18,21 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset= Book.objects.all()
     serializer_class = BookSerializer
 
+class BookListView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        queryset = Book.objects.all()
+        serializer = BookSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class BookCreateView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
