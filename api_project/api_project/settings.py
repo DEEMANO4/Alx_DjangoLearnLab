3e9d6e3ee202dfc1f62850938ce7d922fa14e399
DEEMANO4 <apenadavid18@gmail.com>
2025-11-21 12:07:26 +0100

    ..

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -126,14 +126,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        "rest_framework.authentication.TokenAuthentication"
-    ] 
+        "rest_framework.authentication.TokenAuthentication"] , 
+
+     'DEFAULT_PERMISSION_CLASSES': [
+          'rest_framework.permissions.IsAuthenticated',
+          'rest_framework.permissions.IsAuthorised',
+      ]
+    
         
         
     
     
-}
-# 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    #     'rest_framework.permissions.IsAuthorised',
-    # ]
+ }
