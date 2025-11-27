f8cddb34773c96b2971d154acf2aadec95cfef60
DEEMANO4 <apenadavid18@gmail.com>
2025-11-27 14:52:03 +0100

    views and permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Book
+from django.urls import reverse_lazy
 # Create your views here.
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
+    context_object_name = 'books'
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+
+class BookCreateView(CreateView):
+    model = Book
+    template_name = 'create_book.html'
+    fields = ['title', 'author', 'publication_year']
+    success_url = reverse_lazy('book_list')
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+class BookUpdateView(UpdateView):
+    model = Book
+    template_name = 'create_book.html'
+    fields = ['title', 'author', 'publication_year']
+    success_url = reverse_lazy('book_list')
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'book_confirm_delete.html'
+    success_url = reverse_lazy('book_list')
