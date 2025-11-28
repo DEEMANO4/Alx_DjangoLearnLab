7b61cd0d7211f9155266df059fb6212826878b82
DEEMANO4 <apenadavid18@gmail.com>
2025-11-28 13:59:14 +0100

    Tests

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -9,10 +9,43 @@ class BookAPITestCase(APITestCase):
     def setup(self):
         self.book1 = Book.objects.create(title="Test Book 1", author="Author A", publication_year =2020)
         self.book2 = Book.objects.create(title="Title Book 2", author="Author B", publication_year=2021)
+        self.list_url = reverse('book_list')
+
+
+    def test_get_book_list(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        expected_data = BookSerializer([self.book1, self.book2], many=True)
+        self.assertEqual(response.data, expected_data)
 
 
 
+    def test_get_book_detail(self):
+        detail_url = reverse('book_detail', kwargs={'pk': self.book1.pk}) 
+        response = self.client.get(detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        expected_data = BookSerializer(self.book1).data
+        self.assertEqual(response.data, expected_data)
 
 
+    def test_create_book(self):
+        new_book_data = {'title': 'New Book', 'author': 'New Author', 'publication_year': 2023}
+        response = self.client.post(self.list_url, new_book_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Book.objects.filter(title='New Book').exists())
 
 
+    def test_update_book(self):
+        detail_url = reverse('book-detail', kwargs={'pk': self.book1.pk})
+        updated_data = {'title': 'Updated Title', 'author': 'Author A', 'publication_year': 2020}
+        response = self.client.put(detail_url, updated_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book1.refresh_from_db()
+        self.assertEqual(self.book1.title, 'Updated Title')
+
+
+    def test_delete_book(self):
+        detail_url = reverse('book-detail', kwargs={'pk': self.book1.pk})
+        response = self.client.delete(detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Book.objects.filter(pk=self.book1.pk).exists())  
