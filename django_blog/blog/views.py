1c977127ab303aa4a8adf32f419505ba75788274
DEEMANO4 <apenadavid18@gmail.com>
2025-12-07 14:46:24 +0100

    Search Filter Creation

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -168,7 +168,8 @@ def search_results(request):
 
     if query:
         posts = Post.objects.filter(
-            Q(title_icontains=query) | Q(content_icontains=query) | Q(tags_name_icontains=query)
+            Q(title__icontains=query) | Q(content__icontains=query) |
+            Q(tags__name__icontains=query)
         ).distinct()
 
     context = {
