05695d91b9cf659ba21995d6975526e32ed81333
DEEMANO4 <apenadavid18@gmail.com>
2025-12-07 05:09:47 +0100

    Post Management

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import POST
+from .models import Post
+from django import forms
 
 class PostRegistrationForm(UserCreationForm):
     class Meta:
-        model = POST
+        model = Post
         fields = ('email')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import PostForm
 from .forms import PostRegistrationForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
 
 # Create your views here.
 
@@ -12,3 +18,66 @@ def register_view(request):
     else:
         form = PostRegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post_instance = form.save(commit=False)
+            post_instance.author = request.user
+            post_instance.save()
+            messages.success(request, f'Your post has been created')
+            return redirect(post_instance.get_absolute_url())
+    else:
+        form = PostForm()
+
+    context = {
+        'form': form,
+        'title': 'New Post'
+    }
+    return render(request, 'blog/post_form.html', context)
