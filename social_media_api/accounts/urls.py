f24446b6bae9a3d1fe4382af1b75c43337245cfe
DEEMANO4 <apenadavid18@gmail.com>
2025-12-13 18:15:30 +0100

    following

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('profile/', ProfileManagementView.as_view(), name='profile-management'),
     path('follow/<int:user_id>/', FollowUserView.as_view(), name='followed')
-    path('unfollow/<int:user_id>', UnfollowUserView.as_view(), name='unfollowed')
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollowed')
 ]
