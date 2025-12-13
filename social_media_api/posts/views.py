a0c03640d99ad73891be1a803bbd01d5a0951190
DEEMANO4 <apenadavid18@gmail.com>
2025-12-13 16:29:38 +0100

    Post and Comments viewsets

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,37 +2,65 @@ from django.shortcuts import render
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
+from rest_framework import viewsets, permissions
 
 # Create your views here.
 
-class PostListCreateView(generics.ListCreateApiView):
-    queryset = Post.objects.all()
+class PostViewSet(Viewsets.ModelViewSet):
+    queryset = Post.objects.all
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
-        queryset = Post.objects.all()
-        serializer_class = PostSerializer
-        permission_classes =[permissions.IsAuthenticatedOrReadOnly,IsAuthorOrReadOnly]
-
-
-class CommentListCreateView(generics.ListCreateApiView):
+class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
-    def get_qyeryset(self):
-        post_pk =self.kwargs['post_pk']
-        return Comment.objects.filter(post_pk=post_pk)
+    def get_queryset(self):
+        if 'post_pk' in self.kwargs:
+            return Comment.objects.filter(post_pk=self.kwargs['post_pk'])
+        return Comment.objects.all()
 
     def perform_create(self, serializer):
-        post = Post.objects.get(pk=self.kwargs['post_pk'])
-        serializer.save(author=self.equest.user, post=post)
+        post Post.objects.get(pk=self.kwargs['post_pk'])
+        serializer.save(author=self.request.user, post=post)
 
-class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-    lookup_field = 'pk'
+
+
+
+
+
+
+# class PostListCreateView(generics.ListCreateApiView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user)
+
+#     class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+#         queryset = Post.objects.all()
+#         serializer_class = PostSerializer
+#         permission_classes =[permissions.IsAuthenticatedOrReadOnly,IsAuthorOrReadOnly]
+
+
+# class CommentListCreateView(generics.ListCreateApiView):
+#     serializer_class = CommentSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+#     def get_qyeryset(self):
+#         post_pk =self.kwargs['post_pk']
+#         return Comment.objects.filter(post_pk=post_pk)
+
+#     def perform_create(self, serializer):
+#         post = Post.objects.get(pk=self.kwargs['post_pk'])
+#         serializer.save(author=self.equest.user, post=post)
+
+# class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+#     lookup_field = 'pk'
