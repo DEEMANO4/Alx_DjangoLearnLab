19a95b3e02057b1b7a22197e632f5775f64ce213
DEEMANO4 <apenadavid18@gmail.com>
2025-12-14 07:05:00 +0100

    setting up notifications system

---
+++ social_media_api/notifications/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+# Create your models here.
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=True, related_name='notifications', 
+    help_text='The user designated to receive the notification.')
+    verb = models.CharField(
+        max_length=255, help_text='A short description of the action (e.g "commented", "liked", "followed")'
+    )
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True
+    )
+    is_read = models.BooleanField(default=False, db_index=True)
+
+    actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='actions_peformed', help_text='The user who initiated the action.')
+
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    target_object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('target_content_type,', 'target_object_id')
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"{self.actor} {self.verb} {self.target} to {self.recipient}"
